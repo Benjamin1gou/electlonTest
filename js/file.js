ea64552eff40f4a13b21ed0399ae6a5f731e9267
Tester <yankuma4@gmail.com>
2018-04-30 20:47:45 +0900

    add: テキスト追記処理追加

--- js/file.js
+++ js/file.js
@@ -2,15 +2,26 @@
  * file操作用クラス
  * @param {*} ipcMain 
  */
+const fs = require('fs');
 
-let fileControler = function(ipcMain){
+const fileControler = function(ipcMain){
 
     //検証用
     ipcMain.on('test',function(event, text){
         console.log(text);
     });
 
-    
+    //テキスト追記
+    ipcMain.on('append', function(event, data, name){
+        data = name + ' : ' + data + '\n';
+        fs.appendFile('./files/test.txt', data, function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+    });
+
+
 }
 
 module.exports = fileControler;
